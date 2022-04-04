1269181c6383d1eeb165015f8fca67577fdd068c
stalink456 <stas.litvin2010@yandex.ru>
2022-04-04 17:31:09 +0300

    start project

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNCI1rds38n4dTvFhl87_Qaq_xRLxvztY",
+  authDomain: "house-marketplace-app-9923b.firebaseapp.com",
+  projectId: "house-marketplace-app-9923b",
+  storageBucket: "house-marketplace-app-9923b.appspot.com",
+  messagingSenderId: "926403439676",
+  appId: "1:926403439676:web:9a44cbba96dfdd88a6a739",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
